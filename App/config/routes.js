05cc0ca60a47557e3870e9f713db46ccead6e016
Nick Foden <nickfoden@gmail.com>
2019-07-20 09:39:48 -0400

    Update Spencer Starter

---
+++ App/config/routes.js
@@ -0,0 +1,61 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Home from '../screens/Home';
+import CurrencyList from '../screens/CurrencyList';
+import Options from '../screens/Options';
+import Themes from '../screens/Themes';
+
+const HomeStack = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        header: () => null,
+        headerTitle: 'Home',
+      },
+    },
+    Options: {
+      screen: Options,
+      navigationOptions: {
+        headerTitle: 'Options',
+      },
+    },
+    Themes: {
+      screen: Themes,
+      navigationOptions: {
+        headerTitle: 'Themes',
+      },
+    },
+  },
+  {
+    headerMode: 'screen',
+  },
+);
+
+const CurrencyListStack = createStackNavigator({
+  CurrencyList: {
+    screen: CurrencyList,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: navigation.state.params.title,
+    }),
+  },
+});
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeStack,
+    },
+    CurrencyList: {
+      screen: CurrencyListStack,
+    },
+  },
+  {
+    mode: 'modal',
+    headerMode: 'none',
+  },
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
